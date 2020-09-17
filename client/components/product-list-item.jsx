395ab10f81f72fbcf2b1b66736d4d3111e28cadf
MattWotus <mawotus@gmail.com>
2020-09-17 06:49:52 +0000

    Updated styling and spacing

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -6,13 +6,15 @@ function ProductListItem(props) {
   const price = formatter.format(insertDecimal(props.price));
   const description = props.description;
   return (
-    <div className="col-4" onClick={props.onClick}>
-      <div className="card mb-5">
-        <img src={image} className="card-img-top" alt={product} />
-        <div className="card-body">
-          <h5 className="card-title">{product}</h5>
-          <p className="card-text">{price}</p>
-          <p className="card-text">{description}</p>
+    <div className='col-md-6 col-lg-4 d-flex justify-content-center' onClick={props.onClick}>
+      <div className='card-group'>
+        <div className='card mb-5'>
+          <img src={image} className='card-img-top' alt={product} />
+          <div className='card-body'>
+            <h5 className='card-title mb-2'>{product}</h5>
+            <p className='card-text mb-2 textColor'>{price}</p>
+            <p className='card-text'>{description}</p>
+          </div>
         </div>
       </div>
     </div>
